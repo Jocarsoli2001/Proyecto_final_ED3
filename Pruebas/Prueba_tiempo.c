53a77134f33798c31d98a58063a9d6be9b9e76d3
Jocarsoli2001 <Jocarsoli2001@gmail.com>
2022-11-02 16:16:03 -0600

    Pruebas - ADC y timeofday
    Se añadieron los archivos que incluyen los códigos de conversión de datos por medio de un integrado ADC (por medio de SPI) y el código para utilizar la función gettimeofday().

--- Pruebas/Prueba_tiempo.c
+++ Pruebas/Prueba_tiempo.c
@@ -23,10 +23,13 @@
  |                         VARIABLES GLOBALES                         |
  +--------------------------------------------------------------------+
 */  
-	// Variables respecto a tiempo
-	static int seconds_last = 99;
-	char TimeString[128];
-	struct timeval curTime;
+// Variables respecto a tiempo
+static int seconds_last = 99;
+char TimeString[128];
+
+struct timeval current_time;
+double delta, total, tiempo_inicial, tiempo_actual;
+
 /* 	
  +--------------------------------------------------------------------+
  |                                MAIN                                |
@@ -34,14 +37,25 @@
 */ 
 int main(void)
 {
+	// Iniciar función
+	gettimeofday(&current_time, NULL);
+
+	// Obtener tiempo inicial
+	tiempo_inicial = current_time.tv_sec + current_time.tv_usec/1000000.0;
 
 	while(1)
 	{
-		gettimeofday(&curTime, NULL);
-		seconds_last = curTime.tv_sec;
+		// Obtener valores de gettimeofday()
+		gettimeofday(&current_time, NULL);
+
+		// Generar tiempo total
+		tiempo_actual = current_time.tv_sec + current_time.tv_usec/1000000.0;
+		//printf("Segundos : %ld,  Micro segundos : %ld, Tiempo actual: %lf\n", current_time.tv_sec, current_time.tv_usec, tiempo_actual);
+
+		// Generar un delta
+		delta = (tiempo_actual - tiempo_inicial);
+		printf("Tiempo delta: %f\n", delta);
 		
-		strftime(TimeString, 80, "%Y-%m-%d %H:%M:%S\n", localtime(&curTime.tv_sec));
-		printf("Tiempo actual: %s", TimeString);
 	}
 	return 0;
 }
